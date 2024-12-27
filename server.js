cdd55267e5f5b146fedfbfc6b611972614f58689
Keshav Singh <97099667+Kimforee@users.noreply.github.com>
2024-12-27 23:48:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
+const path = require("path");
 
 // Load environment variables
 dotenv.config();
 
 // Initialize app
 const app = express();
+
+// Serve static files for Swagger UI
+app.use("/api-docs", express.static(path.join(__dirname, "node_modules/swagger-ui-dist")));
 
 // Middleware for parsing JSON
 app.use(express.json());
@@ -23,18 +27,16 @@ app.get('/debug-env', (req, res) => {
   res.json({ mongoUri: process.env.MONGO_URI });
 });
 
-
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
 const userRoutes = require('./routes/userRoutes');
 const rideRoutes = require('./routes/rideRoutes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./utils/swagger');
 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 // Swagger UI setup
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 // Use user routes
